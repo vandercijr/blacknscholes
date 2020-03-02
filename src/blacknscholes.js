0027e961319c2697e0da51fc172b72c65fac0fe9
vanderci curvelo junior <vandercijr@yahoo.com.br>
2020-03-02 19:06:53 -0300

    move gauss functions to gauss file

--- src/blacknscholes.js
+++ src/blacknscholes.js
@@ -1,8 +1,4 @@
-module.exports = {
-	zscore : (x, mu, sigma) => {
-    	return (x - mu) / sigma;
-	},
-	normalizationFactor : (sigma) => {
-	    return 1 / (sigma * Math.sqrt(2 * Math.PI));
-	}
+const blacknscholes = {
 }
+
+module.exports = blacknscholes;

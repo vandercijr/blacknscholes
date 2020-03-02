282063edb29de3e674d5e0aaf922c11545a010b1
vanderci curvelo junior <vandercijr@yahoo.com.br>
2020-03-02 19:18:02 -0300

    add trapezoid method for integration of a function

---
+++ src/integration.js
@@ -0,0 +1,15 @@
+const integration = {
+	trapezoid : (f, a, b, n) => {
+	    let sum = 0
+	    ,deltax = (b-a) / n
+	    ,fafb = (f(a)+f(b))/2;
+
+	    for (let k = 1;k < n;k++) {
+	        sum += f(a + k*deltax);
+	    }
+
+	    return deltax * (fafb + sum); 
+	}
+}
+
+module.exports = integration

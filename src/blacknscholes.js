a605364117152bafe7537d24035f57538d38e5de
vanderci curvelo junior <vandercijr@yahoo.com.br>
2020-03-04 01:11:45 -0300

    add d1 term equation

--- src/blacknscholes.js
+++ src/blacknscholes.js
@@ -1,4 +1,7 @@
 const blacknscholes = {
+	d1Term : (sprice, strike, volatility, irate, rtime) => {
+		return (Math.log(sprice / strike) + (irate + Math.pow(volatility,2)/2) * rtime) / (volatility * Math.sqrt(rtime));
+	}
 }
 
 module.exports = blacknscholes;

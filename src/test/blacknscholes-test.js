454364ce42274cf98520fc6cf611a2416602f9dd
vanderci curvelo junior <vandercijr@yahoo.com.br>
2020-03-04 09:41:17 -0300

    add option greek delta function test

--- src/test/blacknscholes-test.js
+++ src/test/blacknscholes-test.js
@@ -24,7 +24,7 @@ describe('TDD for black & scholes operations', () => {
 	it('Should calculate the theoretical option price equation for a call', () => {
 		const d1 = blacknscholes.d1Term(32,34,0.3,0.0875,0.0822).toFixed(9);
 		const d2 = blacknscholes.d2Term(d1,0.3,0.0822).toFixed(9);
-		assert.equal(blacknscholes.optionPrice(32,34, 0.0875, 0.0822, d1, d2, 'call').toFixed(2), 0.46);
+		assert.equal(blacknscholes.price(32,34, 0.0875, 0.0822, d1, d2, 'call').toFixed(2), 0.46);
 	});	
 
 	it('Should calculate the theoretical option price equation for a put', () => {
@@ -35,8 +35,8 @@ describe('TDD for black & scholes operations', () => {
 	});
 
 	it('Should calculate the option greek delta equation for a call', () => {
-		const d1 = blacknscholes.d1Term(32,34,0.3,0.0875,0.0822).toFixed(9);
-		
-		assert.equal(blacknscholes.delta(d1, 'call').toFixed(5), 2.22344);
+		const d1 = blacknscholes.d1Term(14.06,14,0.2,0.1325,0.03).toFixed(9);
+
+		assert.equal(blacknscholes.delta(d1, 'call').toFixed(4), 0.6008);
 	});		
 });

265ac6993d9e1b4c6ba1fdae8b7cafda5acaa0f2
Vanderci <vandercijr@gmail.com>
2020-03-19 23:49:02 -0300

    add test for gamma eq

--- src/test/blacknscholes-test.js
+++ src/test/blacknscholes-test.js
@@ -12,87 +12,70 @@ const chai = require('chai');
 const blacknscholes = require('../blacknscholes.js');
 const assert = chai.assert;
 const options = {
-	sprice : 46.7,
-	strike : 47.8,
+	stock_price : 38.65,
+	strike : 39.55,
 	volatility : 0.5157,
 	irate : 0.0425,
-	rtime : 0.03,
+	rtime : 0.087671,
 }
 
 describe('TDD for black & scholes operations', () => {
 	it('Should calculate the d1 B&S equation', () => {
-		// options.sprice = 32;
-		// options.strike = 34;
-		// options.volatility = 0.3;
-		// options.irate = 0.0875;
-		// options.rtime = 0.0822;
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
 
-		const d1 = blacknscholes.d1Term(options.sprice, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
-
-		assert.equal(d1, -0.578213940);
+		assert.equal(d1, -0.050001632);
 	});
 
 	it('Should calculate the d2 term B&S equation', () => {
-		// options.sprice = 32;
-		// options.strike = 34;
-		// options.volatility = 0.3;
-		// options.irate = 0.0875;
-		// options.rtime = 0.0822;
-
-		const d1 = blacknscholes.d1Term(options.sprice, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
 		const d2 = blacknscholes.d2Term(d1, options.volatility, options.rtime).toFixed(9);
 
-		assert.equal(d2, -0.664225567);
-	});	
+		assert.equal(d2, -0.202696736);
+	});
 
 	it('Should calculate the theoretical option price equation for a call', () => {
-		// options.sprice = 32;
-		// options.strike = 34;
-		// options.volatility = 0.3;
-		// options.irate = 0.0875;
-		// options.rtime = 0.0822;
-
-		const d1 = blacknscholes.d1Term(options.sprice, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
 		const d2 = blacknscholes.d2Term(d1, options.volatility, options.rtime).toFixed(9);
 
-		assert.equal(blacknscholes.price(options.sprice, options.strike, options.irate, options.rtime, d1, d2, 'call').toFixed(2), 0.46);
-	});	
+		assert.equal(blacknscholes.price(options.stock_price, options.strike, options.irate, options.rtime, d1, d2, 'call').toFixed(2), 2.02);
+	});
 
 	it('Should calculate the theoretical option price equation for a put', () => {
-		// options.sprice = 32;
-		// options.strike = 34;
-		// options.volatility = 0.3;
-		// options.irate = 0.0875;
-		// options.rtime = 0.0822;
-
-		const d1 = blacknscholes.d1Term(options.sprice, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
 		const d2 = blacknscholes.d2Term(d1, options.volatility, options.rtime).toFixed(9);
 
-		assert.equal(blacknscholes.price(options.sprice, options.strike, options.irate, options.rtime, d1, d2, 'put').toFixed(2), 2.22);
+		assert.equal(blacknscholes.price(options.stock_price, options.strike, options.irate, options.rtime, d1, d2, 'put').toFixed(2), 2.77);
 	});
 
 	it('Should calculate the option greek delta equation for a call', () => {
-		// options.sprice = 14.06;
-		// options.strike = 14;
-		// options.volatility = 0.2;
-		// options.irate = 0.1325;
-		// options.rtime = 0.03;
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
 
-		const d1 = blacknscholes.d1Term(options.sprice, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
+		assert.equal(blacknscholes.delta(d1, 'call').toFixed(4), 0.4801);
+	});
 
-		assert.equal(blacknscholes.delta(d1, 'call').toFixed(4), 0.6008);
+  it('Should calculate the option greek delta equation for a put', () => {
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
+
+		assert.equal(blacknscholes.delta(d1, 'put').toFixed(4), -0.5199);
 	});
 
 	it('Should calculate the option greek rho equation for a call', () => {
-		// options.sprice = 25;
-		// options.strike = 25;
-		// options.volatility = 0.2;
-		// options.irate = 0.10;
-		// options.rtime = 0.25;
-
-		const d1 = blacknscholes.d1Term(options.sprice, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
 		const d2 = blacknscholes.d2Term(d1, options.volatility, options.rtime).toFixed(9);
 
-		assert.equal(blacknscholes.rho(options.strike, options.irate, options.rtime, d2, 'call').toFixed(4), 0.6008);
-	});		
+		assert.equal(blacknscholes.rho(options.strike, options.irate, options.rtime, d2, 'call').toFixed(6), 0.014498);
+	});
+
+  it('Should calculate the option greek rho equation for a put', () => {
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
+		const d2 = blacknscholes.d2Term(d1, options.volatility, options.rtime).toFixed(9);
+
+		assert.equal(blacknscholes.rho(options.strike, options.irate, options.rtime, d2, 'put').toFixed(6), -0.020047);
+	});
+
+  it('Should calculate the option greek gamma equation', () => {
+		const d1 = blacknscholes.d1Term(options.stock_price, options.strike, options.volatility, options.irate, options.rtime).toFixed(9);
+
+		assert.equal(blacknscholes.gamma(options.stock_price, options.volatility, options.rtime, d1).toFixed(6), 0.6008);
+	});
 });

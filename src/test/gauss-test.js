51a18cfde2d25e71059457839ffffcaf23a03b8b
Vanderci <vandercijr@gmail.com>
2020-05-30 17:40:18 -0300

    apply lint rules

--- src/test/gauss-test.js
+++ src/test/gauss-test.js
@@ -9,26 +9,25 @@
  */
 
 const chai = require('chai');
-const integration = require('../integration.js');
-const gauss = require('../gauss.js');
+const gauss = require('../math/gauss');
 const assert = chai.assert;
 
 describe('TDD for normal distribution operations', () => {
-	it('Should calculate the zscore of a random variable', () => {
-		assert.equal(gauss.zscore(12, 2, 5), 2);
-		assert.equal(gauss.zscore(10, 1, 5), 1.8);
-	});
+  it('Should calculate the zscore of a random variable', () => {
+    assert.equal(gauss.zscore(12, 2, 5), 2);
+    assert.equal(gauss.zscore(10, 1, 5), 1.8);
+  });
 
-	it('Should calculate the normalization factor for normal distribution formula', () => {
-		assert.equal(gauss.normalizationFactor(1).toFixed(8), 0.39894228);
-		assert.equal(gauss.normalizationFactor(2).toFixed(8), 0.19947114);
-	});
+  it('Should calculate the normalization factor for normal distribution formula', () => {
+    assert.equal(gauss.normalizationFactor(1).toFixed(8), 0.39894228);
+    assert.equal(gauss.normalizationFactor(2).toFixed(8), 0.19947114);
+  });
 
-	it('Should calculate the standard probability density function', () => {
-		assert.equal(gauss.spdf(1).toFixed(8), 0.24197072);
-	});
+  it('Should calculate the standard probability density function', () => {
+    assert.equal(gauss.spdf(1).toFixed(8), 0.24197072);
+  });
 
-	it('Should calculate the cumulative standard normal distribution returning a value from z table', () => {
-		assert.equal(gauss.standardNormalDistribution(1).toFixed(9), 0.841344746);
-	});
+  it('Should calculate the cumulative standard normal distribution returning a value from z table', () => {
+    assert.equal(gauss.standardNormalDistribution(1).toFixed(9), 0.841344222);
+  });
 });
